3f960e4af13066720e524c7bd7072cc36f4b5992
JAJesusGarcia <jesusjagarcia98@gmail.com>
2024-09-22 19:25:46 -0300

    corrección de Contacts

--- front/src/components/Contact/Contact.tsx
+++ front/src/components/Contact/Contact.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   MapPin,
   Phone,
@@ -7,11 +7,48 @@ import {
   Linkedin,
   SquareArrowOutUpRight,
 } from "lucide-react";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Simulate form submission
+    setTimeout(() => {
+      MySwal.fire({
+        title: <p>Message Sent Successfully!</p>,
+        icon: "success",
+      });
+      // Clear form inputs
+      setFormData({
+        name: "",
+        email: "",
+        message: "",
+      });
+    }, 1000); // Simulate a 1-second delay
+  };
+
   return (
     <main>
-      <div className="mx-auto mb-10 mt-10 w-full max-w-6xl rounded-3xl bg-secondary/50 pt-10">
+      <div className="mx-auto my-10 w-full max-w-6xl rounded-3xl bg-secondary/50 pt-10">
         <div className="p-8">
           <h1 className="mb-6 text-4xl font-bold text-quinary">Contact Us</h1>
           <p className="mb-8 text-primary">
@@ -21,7 +58,7 @@ const Contact = () => {
 
           <div className="flex flex-col gap-10 md:flex-row">
             <div className="md:w-1/2">
-              <form className="space-y-4">
+              <form className="space-y-4" onSubmit={handleSubmit}>
                 <div>
                   <label htmlFor="name" className="mb-2 block text-quinary">
                     Name
@@ -29,7 +66,11 @@ const Contact = () => {
                   <input
                     type="text"
                     id="name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
                     className="w-full rounded bg-primary/10 p-2 text-primary"
+                    required
                   />
                 </div>
                 <div>
@@ -39,7 +80,11 @@ const Contact = () => {
                   <input
                     type="email"
                     id="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
                     className="w-full rounded bg-primary/10 p-2 text-primary"
+                    required
                   />
                 </div>
                 <div>
@@ -48,13 +93,17 @@ const Contact = () => {
                   </label>
                   <textarea
                     id="message"
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
                     rows={4}
                     className="w-full rounded bg-primary/10 p-2 text-primary"
+                    required
                   ></textarea>
                 </div>
                 <button
+                  type="submit"
                   className="relative z-10 mt-10 rounded border-2 border-primary bg-primary px-4 py-2 font-bold text-secondary transition-all duration-300 hover:scale-105 hover:border-primary hover:bg-secondary hover:text-primary active:scale-100"
-                  disabled
                 >
                   Send Message
                 </button>
@@ -68,19 +117,20 @@ const Contact = () => {
                 </h2>
                 <div className="space-y-4">
                   <div className="flex items-center">
-                    <MapPin className="mr-2 h-6 w-6 text-primary" />
+                    <MapPin className="mr-2 size-6 text-primary" />
                     <span className="text-primary">
                       Rosario, Santa Fe - Argentina
                     </span>
                   </div>
 
                   <div className="flex items-center">
-                    <SquareArrowOutUpRight className="mr-2 h-6 w-6 text-primary" />
+                    <SquareArrowOutUpRight className="mr-2 size-6 text-primary" />
                     <span className="text-primary">
                       <a
                         className="hover:text-suse-500"
                         href="https://synergy2devs.com"
                         target="_blank"
+                        rel="noopener noreferrer"
                       >
                         www.synergy2devs.com
                       </a>
@@ -96,17 +146,17 @@ const Contact = () => {
 
                 <div className="space-y-4">
                   <div className="flex items-center">
-                    <Phone className="mr-2 h-6 w-6 text-primary" />
+                    <Phone className="mr-2 size-6 text-primary" />
                     <span className="text-primary">(+54) 3416153479</span>
                   </div>
                   <div className="flex items-center">
-                    <Mail className="mr-2 h-6 w-6 text-primary" />
+                    <Mail className="mr-2 size-6 text-primary" />
                     <span className="text-primary">
                       jgarcia@synergy2devs.com
                     </span>
                   </div>
                   <div className="flex items-center">
-                    <Github className="mr-2 h-6 w-6 text-primary" />
+                    <Github className="mr-2 size-6 text-primary" />
                     <a
                       href="https://github.com/JAJesusGarcia"
                       target="_blank"
@@ -117,7 +167,7 @@ const Contact = () => {
                     </a>
                   </div>
                   <div className="flex items-center">
-                    <Linkedin className="mr-2 h-6 w-6 text-primary" />
+                    <Linkedin className="mr-2 size-6 text-primary" />
                     <a
                       href="https://www.linkedin.com/in/jesusjagarcia/"
                       target="_blank"
