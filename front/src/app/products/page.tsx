89964642b4edab350ca7172687bb0714e7d069b5
JAJesusGarcia <jesusjagarcia98@gmail.com>
2024-10-01 22:19:22 -0300

    finish

--- front/src/app/products/page.tsx
+++ front/src/app/products/page.tsx
@@ -1,52 +1,33 @@
-import Card from "@/components/Card/Card";
-import CardBlog from "@/components/CardBlog/CardBlog";
+/** eslint-disable tailwindcss/enforces-shorthand */
 import CardList from "@/components/CardList/CardList";
-import Hero from "@/components/Hero/Hero";
-
-import { getProductsService } from "@/services/productsService";
+import CardProducts from "@/components/CardProducts/CardProducts";
 import { IProduct } from "../interfaces/products";
+import { getProductsService } from "@/services/productsService";
 
-const Page = async () => {
+const page = async () => {
   try {
-    const url = process.env.API_URL + "/products"; // Construyendo la URL de la API a partir de las variables de entorno
-    if (!url) {
-      throw new Error("API_URL is not defined in environment variables"); // Verificación de que la URL de la API está definida
-    }
-
-    const products = await getProductsService(url); // Llamada al servicio para obtener los productos
-
-    // Verificación de que se obtuvieron productos válidos
-    if (!Array.isArray(products) || products.length === 0) {
-      throw new Error("No products found or invalid data format");
-    }
-
-    // Selección aleatoria de 4 productos destacados
-    const featuredProducts = products
-      .sort(() => 0.5 - Math.random())
-      .filter(
-        (product, index, self) =>
-          index === self.findIndex((t) => t.id === product.id),
-      )
-      .slice(0, 4);
+    // Construyendo la URL de la API a partir de las variables de entorno
+    const url = process.env.API_URL + "/products";
+    // Llamada al servicio para obtener los productos
+    const products = await getProductsService(url);
 
     return (
-      <>
-        <Hero />
-        <main className="container">
-          <CardList>
-            {featuredProducts.map((product: IProduct, i: number) => (
-              <Card key={product.id || i} product={product} />
-            ))}
-          </CardList>
-          <CardBlog />
-        </main>
-      </>
+      <main className="container">
+        <div>
+          <h1 className="my-10 text-4xl text-quinary">Last Products</h1>
+        </div>
+        <CardList>
+          {products.map((product: IProduct, i: number) => (
+            <CardProducts key={i} product={product} />
+          ))}
+        </CardList>
+      </main>
     );
   } catch (error) {
     console.error("Error in page component:", error);
-    // Renderizo un mensaje de error en caso de que algo salga mal
+
+    // mensaje de error en caso que algo salga mal
     return (
-      // eslint-disable-next-line tailwindcss/enforces-shorthand
       <div className="mb-10 mt-10 h-full p-5 text-center">
         We are sorry, an error occurred while loading the products. Please,
         Please try again later.
@@ -55,4 +36,4 @@ const Page = async () => {
   }
 };
 
-export default Page;
+export default page;
